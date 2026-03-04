92c37939610f972dc25b49703e80e00411466945
Mohamed L <mohamed.laaraj@viacesi.fr>
2026-03-04 09:19:18 +0100

    offres php

---
+++ offres_data.php
@@ -0,0 +1,24 @@
+<?php
+$offres = [
+    ['id' => 1, 'titre' => 'Développeur Web Junior', 'resume' => 'Stage de 6 mois sur du PHP et du JavaScript.', 'competences' => ['PHP', 'HTML/CSS']],
+    ['id' => 2, 'titre' => 'Designer UX/UI', 'resume' => 'Conception d’interfaces modernes pour une startup.', 'competences' => ['Figma', 'Adobe XD']],
+    ['id' => 3, 'titre' => 'Administrateur Réseaux', 'resume' => 'Maintenance et sécurisation du parc informatique.', 'competences' => ['Cisco', 'Linux']],
+    ['id' => 4, 'titre' => 'Data Analyst', 'resume' => 'Analyse de données de vente et reporting mensuel.', 'competences' => ['SQL', 'Python']],
+    ['id' => 5, 'titre' => 'Développeur Front-End', 'resume' => 'Intégration de maquettes responsive en React.', 'competences' => ['React', 'CSS3']],
+    ['id' => 6, 'titre' => 'Expert Cybersécurité', 'resume' => 'Audit de sécurité et tests d’intrusion.', 'competences' => ['Kali', 'Wireshark']],
+    ['id' => 7, 'titre' => 'Chef de Projet IT', 'resume' => 'Gestion d’une équipe de 5 développeurs en Agile.', 'competences' => ['Scrum', 'Jira']],
+    ['id' => 8, 'titre' => 'Développeur Mobile', 'resume' => 'Création d’une application de livraison sous Flutter.', 'competences' => ['Flutter', 'Dart']],
+    ['id' => 9, 'titre' => 'Consultant Cloud', 'resume' => 'Migration d’infrastructures locales vers AWS.', 'competences' => ['AWS', 'Terraform']],
+    ['id' => 10, 'titre' => 'Développeur Back-End', 'resume' => 'Conception d’API REST robustes avec Node.js.', 'competences' => ['Node.js', 'MongoDB']],
+    ['id' => 11, 'titre' => 'Community Manager', 'resume' => 'Gestion des réseaux sociaux et création de contenu.', 'competences' => ['Canva', 'Ads']],
+    ['id' => 12, 'titre' => 'Développeur Java', 'resume' => 'Maintenance évolutive d’un logiciel bancaire.', 'competences' => ['Java', 'Spring']],
+    ['id' => 13, 'titre' => 'Technicien Support', 'resume' => 'Assistance utilisateur et dépannage hardware.', 'competences' => ['Windows', 'Ticket']],
+    ['id' => 14, 'titre' => 'Analyste SOC', 'resume' => 'Surveillance des alertes de sécurité en temps réel.', 'competences' => ['SIEM', 'Splunk']],
+    ['id' => 15, 'titre' => 'Développeur Python', 'resume' => 'Automatisation de scripts et Intelligence Artificielle.', 'competences' => ['Python', 'Pandas']],
+    ['id' => 16, 'titre' => 'Webmarketeur', 'resume' => 'Optimisation du référencement naturel (SEO).', 'competences' => ['SEO', 'Semrush']],
+    ['id' => 17, 'titre' => 'Intégrateur Web', 'resume' => 'Conversion de fichiers PSD en HTML/CSS valide.', 'competences' => ['SASS', 'Bootstrap']],
+    ['id' => 18, 'titre' => 'Développeur C#', 'resume' => 'Développement de jeux vidéo sous Unity.', 'competences' => ['C#', 'Unity']],
+    ['id' => 19, 'titre' => 'Ingénieur DevOps', 'resume' => 'Mise en place de pipelines CI/CD.', 'competences' => ['Docker', 'Jenkins']],
+    ['id' => 20, 'titre' => 'Product Owner', 'resume' => 'Définition de la vision produit et backlog.', 'competences' => ['Agile', 'Trello']]
+];
+?>
