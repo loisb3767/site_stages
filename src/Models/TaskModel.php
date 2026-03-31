32f3c68bee8af266a20184719f33c74ba608c3de
YasmineHEP <yasmine.hepmutlu@viacesi.fr>
2026-03-31 11:50:12 +0200

    Fix TaskModel

--- src/Models/TaskModel.php
+++ src/Models/TaskModel.php
@@ -651,8 +651,8 @@ class TaskModel extends Model
 
             $stmt = $this->pdo->query($sql);
             return round((float) $stmt->fetchColumn(), 1);
-<<<<<<< HEAD
     }
+
     public function getPaginatedOffresSearch(int $page, int $parPage, string $q): array
     {
         $offset = ($page - 1) * $parPage;
@@ -721,8 +721,6 @@ class TaskModel extends Model
         $stmt->execute();
         return (int) $stmt->fetchColumn();
     }
-=======
-        }
 
         public function deleteOffre(int $id): bool {
             // Supprimer les compétences liées à l'offre
@@ -741,8 +739,5 @@ class TaskModel extends Model
             $stmt = $this->pdo->prepare("DELETE FROM offre WHERE id_offre = :id");
             return $stmt->execute([':id' => $id]);
         }
-
-        
-        
->>>>>>> 3c3516b804553fa1868cc64413be2d238089bfbe
+      
 }
